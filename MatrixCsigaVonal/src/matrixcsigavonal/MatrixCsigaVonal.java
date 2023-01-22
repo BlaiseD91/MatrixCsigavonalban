8257ca8729acb64ba554ac4b81f02678494d661d
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-01-22 23:00:10 +0100

    általános elv fejlesztésének megkezdése, init eljárás változtatása

--- MatrixCsigaVonal/src/matrixcsigavonal/MatrixCsigaVonal.java
+++ MatrixCsigaVonal/src/matrixcsigavonal/MatrixCsigaVonal.java
@@ -33,19 +33,57 @@ public class MatrixCsigaVonal {
     static int[][] matrixInit(int sorok, int oszlopok){
         int[][] matrix = new int[sorok][oszlopok];
         for (int i = 0; i < sorok; i++) {
-            for (int j = 0; j < oszlopok; j++) {
-                matrix[i][j] = 0;
-            }
+            for (int j = 0; j < oszlopok; j++) {  matrix[i][j] = -1; }
         }
+        return matrix;   
+    }//matrixInit
+    
+    static void matrixBejar(int[][] matrix){
+        int szam = 0, sszam = 0, oszam = 0;
         
-        return matrix;
-        
+        //feltöltés balról jobbra
+        for (int oszlop = 0; oszlop < matrix[sszam].length; oszlop++) {
+                matrix[sszam][oszlop] = szam;
+                szam++;
+                if (oszlop == matrix[sszam].length-1)
+                    oszam=oszlop;
+        }
+        sszam++;
+        //feltöltés fentről lefelé
+        for (int sor = sszam; sor < matrix.length; sor++) {
+            matrix[sor][oszam] = szam;
+            szam++;
+            if (sor == matrix.length-1)
+                    sszam=sor;
+        }
+        oszam--;
+        //feltöltés jobbról balra
+        for (int oszlop = oszam; oszlop >-1; oszlop--) {
+            matrix[sszam][oszlop] = szam;
+            szam++;
+            if (oszlop == 0)
+                    oszam=oszlop;
+            
+        }
+        sszam--;
+        //feltöltés lentről felfelé
+        for (int sor = sszam; sor > -1; sor--) {
+            if(matrix[sor][oszam] != -1){
+                sszam=sor-1;
+                break;
+            }
+            matrix[sor][oszam] = szam;
+            szam++;
+        }
+        oszam++;
     }//matrixInit
     
     public static void main(String[] args) {
         
         
         int[][] matrix = matrixInit(5, 5);
+        
+        matrixBejar(matrix);
         tombKiir(matrix);
     }
     
